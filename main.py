256f3be6af9994510803472ad955df72eebdf30b
Вова <kremlin.vova2@gmail.com>
2020-11-15 16:41:35 +0700

    123

--- main.py
+++ main.py
@@ -4,3 +4,12 @@
 #               Soknyshev D. (%)
 print('Choose your category (1 - one subject, 2 - married couple, 3 - single parent). Type only 1 value: ')
 C = input()
+months_list = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+summ = 0
+print('Enter your income for each month:')
+for month in range(12):
+    print(months_list[month])
+    income = int(input())
+    summ += income
+
+print(summ)
