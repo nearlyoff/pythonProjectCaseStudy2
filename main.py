a41f0f980852b5a7767a5de1f2fb54e36f9b1bbb
Вова <kremlin.vova2@gmail.com>
2020-11-15 16:08:01 +0700

    123

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Case-study #2
-# Developers:   Kremlin V. (20%),
-#               Maslyukova P. (60%),
-#               Soknyshev D. (30%)
+# Developers:   Kremlin V. (%),
+#               Maslyukova P. (%),
+#               Soknyshev D. (%)
+print('Choose your category (1 - one subject, 2 - married couple, 3 - single parent). Type only 1 value: ')
+C = input()
